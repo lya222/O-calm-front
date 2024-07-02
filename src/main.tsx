4e3cc4ba2ca8bfb7152d24faca9829ecd9be3926
RipocheGuillaume <156708459+RipocheGuillaume@users.noreply.github.com>
2024-07-02 13:39:09 +0200

    Revert "Route"

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import "./styles/index.scss";
+import App from "./components/elements/Index/Index.tsx";
+import "./index.css";
 
-import { Provider } from "react-redux";
-import store from "./store";
-import Root from "./components/layouts/Root/index.tsx";
-import Home from "./components/pages/Home/Home.tsx";
-import Error from "./components/pages/Error/Error.tsx";
-import CardDetail from "./components/elements/CardDetail/CardDetail.tsx";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />} errorElement={<Error />}>
-      <Route index element={<Home />} />
-      <Route path="/:slug" element={<CardDetail />} />
-      <Route path="/404" element={<Error />} />
-
-      <Route path="*" element={<Error />} />
-    </Route>
-  )
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
