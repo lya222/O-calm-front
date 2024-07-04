a40cc9f5999d852f0b4d267be8210361c9324ad8
RipocheGuillaume <156708459+RipocheGuillaume@users.noreply.github.com>
2024-07-04 17:12:48 +0200

    Revert "Dev"

--- src/@types/Icredentials.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ICredentials { 
-    password: string;
-    email: string;
-  }
-  
--- src/@types/user.d.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id: number;
-  pseudo: string;
-  password: string;
-  email: string;
-}
