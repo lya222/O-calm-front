0b5b990f9d843d502d74685a68314376150235f0
RipocheGuillaume <guillaume-ripoche@laposte.net>
2024-07-13 19:51:20 +0200

    add point on map

--- src/components/elements/Maps/Maps.tsx
+++ src/components/elements/Maps/Maps.tsx
@@ -2,33 +2,42 @@ import { Box } from '@mui/material';
 import './Maps.scss';
 import { Map, APIProvider, Marker } from '@vis.gl/react-google-maps';
 import { useAppSelector } from '../../../hooks/redux';
-import { Iposition } from '../../../@types/Map';
+import CardPlace from '../CardPlace/CardPlace';
+import { useState } from 'react';
+import { Places } from '../../../@types/places';
+import { motion } from 'framer-motion';
 
 function PlaceOnMaps() {
   const positionMap = { lat: 45, lng: 5 };
   const places = useAppSelector((state) => state.places.list);
-  const positions: Iposition[] = places.map((place) => ({
-    lat: parseFloat(place.gps_location_latitude),
-    lng: parseFloat(place.gps_location_longitude),
-  }));
-  console.log('mes positions pour la map', positions);
+
   const apikey = import.meta.env.VITE_API_MAP_KEY;
 
-  // const [markerRef, marker] = useMarkerRef();
+  //function for cardPlace display
+  const [open, setOpen] = useState(false);
+  const [choicePlace, setChoicePlace] = useState<Places>();
+  const handleOpen = async (place: Places) => {
+    if (open) {
+      await setOpen(false);
+      setChoicePlace(place);
+      setOpen(true);
+    } else {
+      setOpen(true);
+      setChoicePlace(place);
+    }
+  };
+  const handleClose = () => setOpen(false);
 
-  // useEffect(() => {
-  //   if (!marker) {
-  //     return;
-  //   }
-
-  //   // do something with marker instance here
-  // }, [marker]);
+  const cardPlaceDisplay = {
+    visible: { x: 0, opacity: 1, rotate: 0 },
+    hidden: { x: 100, opacity: 0, rotate: 45 },
+  };
 
   return (
     <Box
       id="map"
       sx={{
-        mt: 3,
+        mt: 0,
         mb: 20,
         width: 400,
         maxWidth: '100%',
@@ -37,10 +46,36 @@ function PlaceOnMaps() {
         color: 'black',
       }}
     >
+      <motion.div
+        variants={cardPlaceDisplay}
+        animate={open ? 'visible' : 'hidden'}
+        transition={{ duration: 0.3 }}
+        style={{
+          position: 'absolute',
+          top: 100,
+          left: 0,
+          width: '100%',
+          zIndex: 999,
+        }}
+      >
+        {choicePlace && <CardPlace place={choicePlace} index={1} />}
+      </motion.div>
+
       <APIProvider apiKey={apikey}>
         <Map defaultCenter={positionMap} defaultZoom={5}>
-          {positions.map((position, index) => (
-            <Marker key={index} position={position} />
+          {places.map((place, index) => (
+            <>
+              <Marker
+                key={index}
+                position={{
+                  lat: parseFloat(place.gps_location_latitude),
+                  lng: parseFloat(place.gps_location_longitude),
+                }}
+                onClick={() => handleOpen(place as Places)}
+              />
+
+              {/* <Box onClose={handleClose}></Box> */}
+            </>
           ))}
         </Map>
       </APIProvider>
