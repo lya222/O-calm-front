22c84cca6b9849f409d725b78a31731c00dedbb9
RipocheGuillaume <guillaume-ripoche@laposte.net>
2024-07-13 10:47:52 +0200

    push for merge maps

--- essai.js
+++ essai.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const placeData = {
-  description: 'blal',
-  journey: ['bla', 'bla'],
-  name: 'blublu',
-  // slug: 'blal',
-  user_id: 15,
-};
+// const placeData = {
+//   description: 'blal',
+//   journey: ['bla', 'bla'],
+//   name: 'blublu',
+//   // slug: 'blal',
+//   user_id: 15,
+// };
 
-const response = async () => {
-  const response = await axios.delete(`http://165.22.25.11:4000/places/12 `);
-  console.log(
-    "renvoie apres l'enregistrement d'un nouveau lieu",
-    response.data
-  );
-};
+// const response = async () => {
+//   console.log('geolocalisation', usePosition);
+//   // const response = await axios.delete(`http://165.22.25.11:4000/places/12 `);
+//   // console.log(
+//   //   "renvoie apres l'enregistrement d'un nouveau lieu",
+//   //   response.data
+//   // );
+// };
 
-response();
+// response();
