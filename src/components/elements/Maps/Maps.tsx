f5adb13dbe03aa5e1187b7bb55f2f19e677d4f1f
RipocheGuillaume <guillaume-ripoche@laposte.net>
2024-07-13 22:30:49 +0200

    add style on maps

--- src/components/elements/Maps/Maps.tsx
+++ src/components/elements/Maps/Maps.tsx
@@ -1,49 +1,60 @@
 import { Box } from '@mui/material';
 import './Maps.scss';
-import { Map, APIProvider, Marker } from '@vis.gl/react-google-maps';
+import {
+  Map,
+  APIProvider,
+  Marker,
+  MapCameraChangedEvent,
+} from '@vis.gl/react-google-maps';
 import { useAppSelector } from '../../../hooks/redux';
 import CardPlace from '../CardPlace/CardPlace';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Places } from '../../../@types/places';
 import { motion } from 'framer-motion';
 
 function PlaceOnMaps() {
-  const positionMap = { lat: 45, lng: 5 };
   const places = useAppSelector((state) => state.places.list);
-
   const apikey = import.meta.env.VITE_API_MAP_KEY;
 
-  //function for cardPlace display
+  const [positionMap, setPositionMap] = useState({ lat: 45, lng: 5 });
+  const [zoomMap, setZoomMap] = useState(5);
   const [open, setOpen] = useState(false);
-  const [choicePlace, setChoicePlace] = useState<Places>();
-  const handleOpen = async (place: Places) => {
-    if (open) {
-      await setOpen(false);
-      setChoicePlace(place);
-      setOpen(true);
-    } else {
-      setOpen(true);
-      setChoicePlace(place);
-    }
+  const [choicePlace, setChoicePlace] = useState<Places | null>(null);
+
+  const handleOpen = (place: Places) => {
+    setChoicePlace(place);
+    setPositionMap({
+      lat: parseFloat(place.gps_location_latitude),
+      lng: parseFloat(place.gps_location_longitude),
+    });
+    setZoomMap(12);
+    setOpen(true);
   };
-  const handleClose = () => setOpen(false);
 
   const cardPlaceDisplay = {
     visible: { x: 0, opacity: 1, rotate: 0 },
     hidden: { x: 100, opacity: 0, rotate: 45 },
   };
 
+  const handleCameraChange = useCallback((ev: MapCameraChangedEvent) => {
+    const { center, zoom } = ev.detail;
+    setPositionMap(center);
+    setZoomMap(zoom);
+  }, []);
+
   return (
     <Box
       id="map"
       sx={{
         mt: 0,
-        mb: 20,
-        width: 400,
+        mb: 60,
+        width: '100%',
         maxWidth: '100%',
-        p: 2,
+        height: '600px',
+
         bgcolor: 'white',
         color: 'black',
+        position: 'relative',
       }}
     >
       <motion.div
@@ -51,8 +62,7 @@ function PlaceOnMaps() {
         animate={open ? 'visible' : 'hidden'}
         transition={{ duration: 0.3 }}
         style={{
-          position: 'absolute',
-          top: 100,
+          top: 0,
           left: 0,
           width: '100%',
           zIndex: 999,
@@ -62,20 +72,20 @@ function PlaceOnMaps() {
       </motion.div>
 
       <APIProvider apiKey={apikey}>
-        <Map defaultCenter={positionMap} defaultZoom={5}>
+        <Map
+          center={positionMap}
+          zoom={zoomMap}
+          onCameraChanged={handleCameraChange}
+        >
           {places.map((place, index) => (
-            <>
-              <Marker
-                key={index}
-                position={{
-                  lat: parseFloat(place.gps_location_latitude),
-                  lng: parseFloat(place.gps_location_longitude),
-                }}
-                onClick={() => handleOpen(place as Places)}
-              />
-
-              {/* <Box onClose={handleClose}></Box> */}
-            </>
+            <Marker
+              key={index}
+              position={{
+                lat: parseFloat(place.gps_location_latitude),
+                lng: parseFloat(place.gps_location_longitude),
+              }}
+              onClick={() => handleOpen(place as Places)}
+            />
           ))}
         </Map>
       </APIProvider>
